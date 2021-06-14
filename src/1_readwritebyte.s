126f9424b148594b266994fd5d1f05af108c58d9
Ralf Uhlig <ralf@uhlig-online.de>
2021-06-14 21:37:24 +0200

    Quick and dirty port of hwconfig.s and 1_readwritebyte.s to Corsham KIM Clone. Removed LCD control, SD card control via PORTB of left 6532, output to TTY.

--- src/1_readwritebyte.s
+++ src/1_readwritebyte.s
@@ -1,48 +1,43 @@
-PORTB = $6000
-PORTA = $6001
-DDRB = $6002
-DDRA = $6003
+; KIM 6532 Aux I/O
+PORTB = $1702
+DDRB = $1703
 
-E  = %10000000
-RW = %01000000
-RS = %00100000
+; KIM routines
+OUTCH  = $1EA0 
+PRTBYT = $1E3B
+PRCRLF = $1E2F
 
 SD_CS   = %00010000
 SD_SCK  = %00001000
 SD_MOSI = %00000100
 SD_MISO = %00000010
 
-PORTA_OUTPUTPINS = E | RW | RS | SD_CS | SD_SCK | SD_MOSI
+PORTB_OUTPUTPINS = SD_CS | SD_SCK | SD_MOSI
 
-
-  .org $e000
+  .org $0200
 
 reset:
+  cld
   ldx #$ff
   txs
 
-  lda #%11111111          ; Set all pins on port B to output
+  lda #PORTB_OUTPUTPINS   ; Set various pins on port A to output
   sta DDRB
-  lda #PORTA_OUTPUTPINS   ; Set various pins on port A to output
-  sta DDRA
-
-  jsr lcd_init
-
 
   ; Let the SD card boot up, by pumping the clock with SD CS disabled
-
+  jsr PRCRLF
   lda #'I'
   jsr print_char
 
   ; We need to apply around 80 clock pulses with CS and MOSI high.
-  ; Normally MOSI doesn't matter when CS is high, but the card is
+  ; Normally MOSI does not matter when CS is high, but the card is
   ; not yet is SPI mode, and in this non-SPI state it does care.
 
   lda #SD_CS | SD_MOSI
   ldx #160               ; toggle the clock 160 times, so 80 low-high transitions
 .preinitloop:
   eor #SD_SCK
-  sta PORTA
+  sta PORTB
   dex
   bne .preinitloop
   
@@ -61,7 +56,7 @@ reset:
   jsr print_hex
 
   lda #SD_MOSI           ; pull CS low to begin command
-  sta PORTA
+  sta PORTB
 
   ; CMD0, data 00000000, crc 95
   lda #$40
@@ -83,27 +78,26 @@ reset:
   jsr print_hex
 
   lda #SD_CS | SD_MOSI   ; set CS high again
-  sta PORTA
+  sta PORTB
 
   ; Expect status response $01 (not initialized)
   pla
   cmp #$01
   bne .initfailed
 
-
   lda #'Y'
   jsr print_char
 
-  ; loop forever
+
 .loop:
-  jmp .loop
+.stop:
+  brk
 
 
 .initfailed
   lda #'X'
   jsr print_char
   jmp .loop
-
 
 
 sd_readbyte:
@@ -114,12 +108,12 @@ sd_readbyte:
 .loop:
 
   lda #SD_MOSI                ; enable card (CS low), set MOSI (resting state), SCK low
-  sta PORTA
+  sta PORTB
 
   lda #SD_MOSI | SD_SCK       ; toggle the clock high
-  sta PORTA
+  sta PORTB
 
-  lda PORTA                   ; read next bit
+  lda PORTB                   ; read next bit
   and #SD_MISO
 
   clc                         ; default to clearing the bottom bit
@@ -148,13 +142,13 @@ sd_writebyte:
   tay                         ; save remaining bits for later
 
   lda #0
-  bcc .sendbit                ; if carry clear, don't set MOSI for this bit
+  bcc .sendbit                ; if carry clear, do not set MOSI for this bit
   ora #SD_MOSI
 
 .sendbit:
-  sta PORTA                   ; set MOSI (or not) first with SCK low
+  sta PORTB                   ; set MOSI (or not) first with SCK low
   eor #SD_SCK
-  sta PORTA                   ; raise SCK keeping MOSI the same, to send the bit
+  sta PORTB                   ; raise SCK keeping MOSI the same, to send the bit
 
   tya                         ; restore remaining bits to send
 
@@ -171,81 +165,8 @@ sd_waitresult:
   beq sd_waitresult
   rts
 
-
-lcd_wait:
-  pha
-  lda #%00000000  ; Port B is input
-  sta DDRB
-.busy:
-  lda #RW
-  sta PORTA
-  lda #(RW | E)
-  sta PORTA
-  lda PORTB
-  and #%10000000
-  bne .busy
-
-  lda #RW
-  sta PORTA
-  lda #%11111111  ; Port B is output
-  sta DDRB
-  pla
-  rts
-
-lcd_instruction:
-  jsr lcd_wait
-  sta PORTB
-  lda #0         ; Clear RS/RW/E bits
-  sta PORTA
-  lda #E         ; Set E bit to send instruction
-  sta PORTA
-  lda #0         ; Clear RS/RW/E bits
-  sta PORTA
-  rts
-
-
-lcd_init:
-  lda #%00111000 ; Set 8-bit mode; 2-line display; 5x8 font
-  jsr lcd_instruction
-  lda #%00001110 ; Display on; cursor on; blink off
-  jsr lcd_instruction
-  lda #%00000110 ; Increment and shift cursor; don't shift display
-  jsr lcd_instruction
-
-lcd_cleardisplay:
-  lda #%00000001 ; Clear display
-  jmp lcd_instruction
-
-
 print_char:
-  jsr lcd_wait
-  sta PORTB
-  lda #RS         ; Set RS; Clear RW/E bits
-  sta PORTA
-  lda #(RS | E)   ; Set E bit to send instruction
-  sta PORTA
-  lda #RS         ; Clear E bits
-  sta PORTA
-  rts
+  jmp OUTCH
 
 print_hex:
-  pha
-  ror
-  ror
-  ror
-  ror
-  jsr print_nybble
-  pla
-print_nybble:
-  and #15
-  cmp #10
-  bmi .skipletter
-  adc #6
-.skipletter
-  adc #48
-  jsr print_char
-  rts
-
-  .org $fffc
-  .word reset
-  .word $0000
+  jmp PRTBYT
